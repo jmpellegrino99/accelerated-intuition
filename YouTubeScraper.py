32e2b5557e047bb369783a3522a2e1febe1d1933
J. M. Pellegrino <88442315+jmpellegrino99@users.noreply.github.com>
2021-08-10 16:30:47 -0400

    Update YouTubeScraper.py

--- YouTubeScraper.py
+++ YouTubeScraper.py
@@ -1,11 +1,39 @@
 #NEW SECTION
 #Search Volume Proxies (SVP):
+#Import packages:
 from youtubesearchpython import *
 import pandas as pd
-queries = []
+#Define function for average number of subscribers:
+def remove(test_list):
+    result = 0
+    test_list = test_list.replace(' subscribers', '')
+    if 'K' in list(test_list):
+        result += float(test_list.replace('K',''))*1000
+    elif 'M' in list(test_list):
+        result += float(test_list.replace('M', ''))*1000000
+    else:
+        result += float(test_list)
+    return result
+def avg_subscribers(video):
+    limit_actual = 5
+    video = VideosSearch(video,limit = limit_actual).result(mode=ResultMode.dict).get('result')
+    empty_list_test = []
+    try:      
+        for i in range(len(video)):
+            empty_list_test.append(video[i].get('channel').get('name'))
+        average = 0
+        for element in empty_list_test:
+            average += float(remove(ChannelsSearch(element,limit=1,region='US').result().get('result')[0].get('subscribers')))
+        average = average/limit_actual
+        return average
+    except: 
+        return "NaN"
+#Begin entering queries and specifying parameters
+queries = ['honda accord', 'honda civic', 'honda odyssey', 'honda pilot']
 limit_actual = 10
 average_views = []
 average_duration = []
+average_subscribers = []
 try:
     for query in queries: 
         return_list_1 = []
@@ -30,24 +58,29 @@ try:
         w2 = z2/limit_actual
         average_views.append(w1)
         average_duration.append(w2)
+        average_subscribers.append(int(avg_subscribers(query)))
     query_tally = []
     view_tally = []
     duration_tally = []
     for i in range(len(queries)):
         query_tally.append(queries[i])
     for i in range(len(queries)):
-        view_tally.append(round(average_views[i],0))
+        view_tally.append(int(average_views[i]))
     for i in range(len(queries)):
         duration_tally.append(round(average_duration[i],1))
-    rsvp = pd.DataFrame(columns = ['Query', 'Views', 'Duration'])
+    rsvp = pd.DataFrame(columns = ['Query', 'Views', 'Duration', 'Subscribers'])
     rsvp.Query = query_tally
     rsvp.Views = view_tally
     rsvp.Duration = duration_tally
+    rsvp.Subscribers = average_subscribers
 #if an error is thrown, the name of the "problem" query will be printed
 except:
     print(queries[len(average_views)])
 #return the data frame
 rsvp
+
+
+
 
 
 
@@ -78,14 +111,13 @@ table.Rank = rank_order
 table.Video = titles_list
 table
 
-#MEDIAN FUNCTION:
-def median(list1):
-    list1 = list(set(list1))
-    if len(list1) % 2 == 0:
-        return list1[len(list1)//2-1]/2 + list1[len(list1)//2]/2
-    if len(list1) % 2 != 0:
-        return list1[(len(list1)-1)//2]		
 
+
+
+
+
+#NEW SECTION
+#MISCELLANEOUS (ideas in progress/scratch work):
 #HOW TO GET SUGGESTED TERMS:
 def related(queries):
     output_list = []
@@ -95,72 +127,3 @@ def related(queries):
         for element2 in suggestion:
             output_list.append(element2)
     return output_list
-
-
-#NEW SECTION
-#MISCELLANEOUS (ideas in progress/scratch work)
-
-#SETTING UP A DATA FRAME:
-rsvp = pd.DataFrame(columns = ['Query', 'Views', 'Duration'])
-rsvp.Query = query_tally
-rsvp.Views = view_tally
-rsvp.Duration = duration_tally
-rsvp
-
-#HOW TO GET CHANNEL SUBSCRIBER COUNT:
-def remove(test_list):
-    result = 0
-    test_list = test_list.replace(' subscribers', '')
-    if 'K' in list(test_list):
-        result += float(test_list.replace('K',''))*1000
-    if 'M' in list(test_list):
-        result += float(test_list.replace('M', ''))*1000000
-    else:
-        result += float(test_list)
-    return result
-
-#GETTING CHANNEL NAMES:
-video = VideosSearch('used cars',limit=2).result(mode=ResultMode.dict).get('result')
-empty_list_test = []
-for i in range(len(video)):
-    empty_list_test.append(video[i].get('channel').get('name'))
-for element in empty_list_test:
-    print(ChannelsSearch(element,limit=1,region='US').result().get('result')[0].get('subscribers'))
-
-#GETTING SUBSCRIBER COUNTS FOR CHANNELS CORREPSONDING TO TOP 10 VIDEOS:
-queries_list = ['Honda Civic', 'Honda Fit', 'Honda Accord']
-average_subscribers = []
-for item in queries_list:
-    channel_names = []
-    subscribers = []
-    for i in range(10):
-        channel_names.append(VideosSearch(item,limit=10).result(mode=ResultMode.dict).get('result').get('channel').get('name'))
-    for i in range(10):
-        subscribers.append(remove(ChannelsSearch(channel_names[i],limit=1,region='US').result().get('result')[0].get('subscribers'))
-    sum = 0
-    for number in subscribers:
-        sum += number 
-    average_subscribers.append(sum//10)
-    
-#IN PROGRESS FOR COMPUTING SUBSCRIBER COUNTS:
-def remove(test_list):
-    result = 0
-    test_list = test_list.replace(' subscribers', '')
-    if 'K' in list(test_list):
-        result += float(test_list.replace('K',''))*1000
-    elif 'M' in list(test_list):
-        result += float(test_list.replace('M', ''))*1000000
-    else:
-        result += float(test_list)
-    return result
-limit_actual = 5
-video_ = str(input("Video: "))
-video = VideosSearch(video_,limit = limit_actual).result(mode=ResultMode.dict).get('result')
-empty_list_test = []
-for i in range(len(video)):
-    empty_list_test.append(video[i].get('channel').get('name'))
-average = 0
-for element in empty_list_test:
-    average += float(remove(ChannelsSearch(element,limit=1,region='US').result().get('result')[0].get('subscribers')))
-average = average/limit_actual
-average
